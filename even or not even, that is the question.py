e33cb8dc96b92758819dc8693213ab3472bf756f
Léa Bertocchi <bertocchileadu32@gmail.com>
2018-09-16 20:02:29 +0200

    Add files via upload

---
+++ even or not even, that is the question.py
@@ -0,0 +1,10 @@
+print("Entre un nombre et je te dis si il est pair ou non")
+nombre = input("-> ")
+nombre=int(nombre)
+
+if nombre%2 == 1:
+    print("Ce nombre est impair")
+else:
+    print("ce nombre est pair!")
+    
+input(" fin du programme ")
